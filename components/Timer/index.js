bdcc498336df95608515e33b11abde41dc6c8b8e
System Administrator <root@jeong-yunhoui-iMac.local>
2019-04-30 13:15:21 +0900

    Init React-Native Timer

---
+++ components/Timer/index.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet, StatusBar} from 'react-native'
+import Button from '../Button'
+
+export default class Timer extends Component
+{
+    render()
+    {
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle={"light-content"}/>
+                <View style={styles.upper}>
+                    <Text style={styles.time}>25:00</Text>
+                </View>
+                <View style={styles.lower}>
+                    <Button iconName="play-circle" onPress={() => alert("It Works !")}/>
+                    <Button iconName="stop-circle" onPress={() => alert("It Works !")}/>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#999",
+    },
+
+    upper: {
+        flex: 2,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    lower: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    time:{
+        color: "white",
+        fontSize: 120,
+        fontWeight: "100"
+    },
+})
